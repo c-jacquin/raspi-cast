5063a1226b51dcfe256afeee1bd1a340f8e579f8
Charles Jacquin <charles.jacquin@autistici.org>
2018-05-20 23:58:22 +0200

    add InitialState

--- packages/raspi-cast-common/index.ts
+++ packages/raspi-cast-common/index.ts
@@ -2,5 +2,6 @@ export * from './enums/CastType';
 export * from './enums/Error';
 export * from './enums/PlaybackStatus';
 
-export * from './interfaces/CastMeta'
-export * from './interfaces/CastOptions'
+export * from './interfaces/CastMeta';
+export * from './interfaces/CastOptions';
+export * from './interfaces/InitialState';
